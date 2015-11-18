521687fc40befbdae217339e4b128c2e61297686
imad0708 <imadmunir@yahoo.com>
2015-11-18 11:26:41 +0100

    map with markers

--- data.js
+++ data.js
@@ -1,14 +1,36 @@
 
 
       function initialize() {
+          var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+          var labelIndex = 0;
+
         var mapCanvas = document.getElementById('map');
+          var lat=new google.maps.LatLng(59.9500, 10.7500);
         var mapOptions = {
-          center: new google.maps.LatLng(59.9500, 10.7500),
+          center:lat,
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }
+          var map = new google.maps.Map(mapCanvas, mapOptions)
+        //adding click event
+          google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+//add marker
+          addMarker(lat, map);
+
+          function addMarker(lat, map) {
+
+      
+         var marker = new google.maps.Marker({
+    position: lat,
+    map: map,
+             label: labels[labelIndex++ % labels.length],
+    title: 'Hello World!'
+  });
+          }
+        }
       google.maps.event.addDomListener(window, 'load', initialize);
         
-    
+
+
