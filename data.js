6fc26d3edd5a03916c5a40f4fa5338ee4cc1634f
imad0708 <imadmunir@yahoo.com>
2015-11-21 10:59:21 +0100

    Data Access Works

--- data.js
+++ data.js
@@ -1,4 +1,18 @@
 
+function init(){
+    	var apiUrl = "http://193.157.207.12:8080/api/organisationUnits.json";
+
+//192.168.0.105193.157.207.12
+    	// Cross-site redirect error solution: Run chrome with --disable-web-security
+    	var base64 = "YWRtaW46ZGlzdHJpY3Q=";
+    	$.ajax(apiUrl, {headers: {'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q='}}).
+    	success(function(data) {
+				alert('success');
+    	}).
+    	error(function(data, status, headers, config) {
+    		alert("Error. Data: " + data);
+    	});
+    }
 
       function initialize() {
           var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
