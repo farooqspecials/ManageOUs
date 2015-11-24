d6ac48e328e828be1bcb3b49acaf42df77e0ea66
farooqspecials <farooqspecials@gmail.com>
2015-11-24 21:12:06 +0100

    OU filter

--- myUsers.js
+++ myUsers.js
@@ -1,19 +1,5 @@
 angular.module('myApp', ['ui.bootstrap']).controller('ApplicationController', function($scope,$http) {
- 
-   /* $scope.filters = {
-        
-        search: ''
-    };
-    
-    $scope.actions = {
-      function () {
-           
-                 $scope.filters.search = '';   
-            
-        }
-    };*/
-    
-    
+  
 $scope.fName = '';
 $scope.lName = '';
 $scope.passw1 = '';
@@ -22,14 +8,14 @@ $scope.IsVisible = false;
    
                 //If DIV is visible it will be hidden and vice versa.
                
-init();           
-    
+init();    
+
 
 function init(){
-    	var apiUrl = "http://192.168.0.105:8082/api/organisationUnits.json?fields=%3Aidentifiable%2Ccoordinates%2Clevel%2CshortName%2Cparent&pageSize=2000";
+    	var apiUrl = "http://localhost:8080/api/organisationUnits.json?fields=:identifiable,coordinates,level,shortName,parent&pageSize=2000";
 		
     	// Cross-site redirect error solution: Run chrome with --disable-web-security
-    	var base64 = "YWRtaW46ZGlzdHJpY3Q=";
+    	//var base64 = "YWRtaW46ZGlzdHJpY3Q=";
     	/*
 		$http.get(apiUrl, {headers: {'Authorization': 'Basic YWRtaW46ZGlzdHJpY3Q='}}).
     	success(function(data) {
@@ -46,7 +32,50 @@ function init(){
 			  console.log(response.organisationUnits);
 		  });
     } 
-$scope.editUser = function(id) {
+    
+    
+    
+    var appctrl = this;
+	appctrl.levelOptions = [{name:'ChiefDom', level:1},{name:'District', level:2},{name:'Organisation Unit', level:3}, 	                         {name:'Facility', level:4}];
+    
+    // Default is Facility.
+	appctrl.currentOrgType = appctrl.levelOptions[3];
+    
+   
+    // A custom filter for the ng-repeat.
+	$scope.customFilter = function(name) {
+		// Custom filter. Currently filtering:
+		// - By level.
+		// - Query.
+
+		return function(orgUnit) {
+			return (orgUnit.level == appctrl.currentOrgType.level)
+               // && orgUnit.name.indexOf(appctrl.currentQuery) != -1);
+		}
+	}
+    
+    
+    $scope.levelOU=function(){
+        
+        appctrl.currentOrgType = appctrl.levelOptions[2];
+    }
+    
+     $scope.levelCD=function(){
+        
+        appctrl.currentOrgType = appctrl.levelOptions[0];
+    }
+    
+     $scope.levelD=function(){
+        
+        appctrl.currentOrgType = appctrl.levelOptions[1];
+    }
+      $scope.levelF=function(){
+        
+        appctrl.currentOrgType = appctrl.levelOptions[3];
+    }
+
+    
+    $scope.editUser = function(id) {
     
     $scope.IsVisible = true;
     
